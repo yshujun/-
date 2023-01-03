b5b59c9570dec8c0d2f8d3054cd1c6b2dd2647ad
yshujun <771145708@qq.com>
2023-01-03 11:19:47 +0800

    需求文档

---
+++ 需求文档/暂估应收及开票申请 (2)/暂估应收及开票申请/files/应收对账单/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,k)),m,[],n,_(h,o),p,[q,r],s,_(t,u,v,w,g,x,y,_(),z,[],A,_(B,C,D,E,F,_(G,H,I,J),K,null,L,M,M,N,O,P,null,Q,R,S,T,U,V,W,R,X,Y,Z,_(G,H,I,ba),bb,R,bc,Y,bd,_(be,bf,bg,bh,bi,bh,bj,bh,bk,k,I,_(bl,bm,bn,bm,bo,bm,bp,bq)),i,_(j,k,l,k)),br,_(),bs,_(),bt,_(bu,[])),bv,_(),bw,_());}; 
+var b="url",c="应收对账单.html",d="generationDate",e=new Date(1662254818381.34),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=0,l="height",m="adaptiveViews",n="sketchKeys",o="s0",p="variables",q="OnLoadVariable",r="data",s="page",t="packageId",u="5b2a59e3954d4b5f9869f8bd403c554c",v="type",w="Axure:Page",x="应收对账单",y="notes",z="annotations",A="style",B="baseStyle",C="627587b6038d43cca051c114ac41ad32",D="pageAlignment",E="center",F="fill",G="fillType",H="solid",I="color",J=0xFFFFFFFF,K="image",L="imageAlignment",M="near",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="Applied Font",W="borderWidth",X="borderVisibility",Y="all",Z="borderFill",ba=0xFF797979,bb="cornerRadius",bc="cornerVisibility",bd="outerShadow",be="on",bf=false,bg="offsetX",bh=5,bi="offsetY",bj="blurRadius",bk="spread",bl="r",bm=0,bn="g",bo="b",bp="a",bq=0.349019607843137,br="adaptiveStyles",bs="interactionMap",bt="diagram",bu="objects",bv="masters",bw="objectPaths";
+return _creator();
+})());
